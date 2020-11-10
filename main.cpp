e851c2cf06901cc940790c21e0895954cc92d467
Jay <47358206+jay-oliver@users.noreply.github.com>
2020-11-10 18:52:41 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,41 +1,48 @@
 #include <Arduino.h>
 #include <Romi32U4.h>
-#include "chassis.h"
+//sensors
+#include "IR_sensor.h"
+#include "Sonar_sensor.h"
+#include "Encoders.h"
+//behaviors
+#include "Speed_controller.h"
+#include "Wall_following_controller.h"
 
-
+enum ROBOT_STATE {ROBOT_IDLE, ROBOT_DRIVING};
 ROBOT_STATE robot_state = ROBOT_IDLE;
-RomiChassis chassis = RomiChassis();
-Romi32U4ButtonA buttonA = Romi32U4ButtonA();
 
-
+Romi32U4ButtonA buttonA; 
+SpeedController PIcontroller;
+WallFollowingController PDcontroller;
+IRsensor irSensor;
 
 void setup() {
-    Serial.begin(115200);
+  PIcontroller.Init();
+  PDcontroller.Init();
 }
 
-void loop() 
-{
+void loop() {
   switch(robot_state)
   {
     case ROBOT_IDLE:
-      if(buttonA.getSingleDebouncedRelease()) 
-      {
-        chassis.StartDriving(100, 100, 10000); //contains your program that the robot executes when pushbutton A is pressed
-        robot_state = ROBOT_DRIVING;
-      }
+      if(buttonA.getSingleDebouncedRelease()) robot_state = ROBOT_DRIVING;
       break;
 
     case ROBOT_DRIVING:
-      chassis.MotorControl();
-      if(chassis.CheckDriveComplete()) 
-      {
-        chassis.Stop();
-        robot_state = ROBOT_IDLE;
-      }
+      //uncomment this line to check whether the speed controller is operational on your robot
+      PIcontroller.Process(50,50);
+      irSensor.PrintData();
+
+      //uncomment this line of code, once you are done with assignments 1 and 2 to demonstrate that your robot
+      //is capable of following a wall autonomously.
+      //int speed = PDcontroller.Start(30); //distance in [cm]
+      //PIcontroller.Start(50+speed,50-speed); //speed in [[mm/s]]
+
       if(buttonA.getSingleDebouncedRelease()) 
       {
-        chassis.Stop();
+        PIcontroller.Stop();
         robot_state = ROBOT_IDLE;
       }
+      break;
   }
 }
