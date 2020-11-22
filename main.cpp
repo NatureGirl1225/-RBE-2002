e534f6d9f9a5974b81ebf88c51ba9f3df0cf93ba
Jay <47358206+jay-oliver@users.noreply.github.com>
2020-11-22 00:40:56 -0500

    Add files via upload
    set up to read z acceleration i hope

--- main.cpp
+++ main.cpp
@@ -1,41 +1,19 @@
 #include <Arduino.h>
 #include <Romi32U4.h>
-#include "chassis.h"
+#include "Behaviors.h"
+#include "Speed_controller.h"
+#include "IMU.h"
 
-
-ROBOT_STATE robot_state = ROBOT_IDLE;
-RomiChassis chassis = RomiChassis();
-Romi32U4ButtonA buttonA = Romi32U4ButtonA();
-
-
+Behaviors collisionBehavior;
+SpeedController controller;
+IMU_sensor sensor;
 
 void setup() {
-    Serial.begin(115200);
+  //collisionBehavior.Init();
 }
 
-void loop() 
-{
-  switch(robot_state)
-  {
-    case ROBOT_IDLE:
-      if(buttonA.getSingleDebouncedRelease()) 
-      {
-        chassis.StartDriving(100, 100, 10000); //contains your program that the robot executes when pushbutton A is pressed
-        robot_state = ROBOT_DRIVING;
-      }
-      break;
-
-    case ROBOT_DRIVING:
-      chassis.MotorControl();
-      if(chassis.CheckDriveComplete()) 
-      {
-        chassis.Stop();
-        robot_state = ROBOT_IDLE;
-      }
-      if(buttonA.getSingleDebouncedRelease()) 
-      {
-        chassis.Stop();
-        robot_state = ROBOT_IDLE;
-      }
-  }
+void loop() {
+  //collisionBehavior.Run();
+  //controller.Turn(90, true);
+  sensor.PrintAcceleration();
 }
